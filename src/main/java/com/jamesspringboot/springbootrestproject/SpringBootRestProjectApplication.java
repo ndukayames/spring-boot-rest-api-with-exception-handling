870c6b6c67426beab4c70c5fb49d05b614bec393
Obi James Onyemaechi <jamesobi@softprism.org>
2022-11-03 22:50:17 +0100

    Simple CRUD Operations with Error Handling

---
+++ src/main/java/com/jamesspringboot/springbootrestproject/SpringBootRestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jamesspringboot.springbootrestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamesspringboot/springbootrestproject/SpringBootRestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesspringboot.springbootrestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
